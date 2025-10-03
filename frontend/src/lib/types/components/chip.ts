5a00bb92414598c29d9e8655ec7d243aa20a1499
LIARA <liarabruchmann@gmail.com>
2025-10-03 21:45:29 +0200

    style: lint

--- frontend/src/lib/types/components/chip.ts
+++ frontend/src/lib/types/components/chip.ts
@@ -1,10 +1,10 @@
-import type { Snippet } from "svelte"
-import type { Color } from "../color"
+import type { Snippet } from 'svelte'
+import type { Color } from '../color'
 
 export type ChipVariant = 'subtle' | 'solid'
 
 export type ChipProps = {
-    children: Snippet
-    color: Color
-    variant?: ChipVariant
+  children: Snippet
+  color: Color
+  variant?: ChipVariant
 }
