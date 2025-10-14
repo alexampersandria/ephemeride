ece8404965f32c3b8e5ec4fbd94adcf2ca56eda6
LIARA <liarabruchmann@gmail.com>
2025-10-14 15:40:48 +0200

    docs: entry docs example

--- frontend/src/lib/types/log.ts
+++ frontend/src/lib/types/log.ts
@@ -31,3 +31,30 @@ export type Entry = {
 export type NewEntry = Omit<Entry, 'id'>
 
 export const entryMaxLength = 1000
+
+/**
+ * see /backend/src/services/log.rs
+ */
+export const defaultCategories: CategoryWithTags[] = [
+  {
+    id: crypto.randomUUID(),
+    name: 'Activities',
+    tags: [
+      { id: crypto.randomUUID(), name: 'Work', color: 'base' },
+      { id: crypto.randomUUID(), name: 'Movie', color: 'base' },
+      { id: crypto.randomUUID(), name: 'Exercise', color: 'base' },
+      { id: crypto.randomUUID(), name: 'Read', color: 'base' },
+      { id: crypto.randomUUID(), name: 'Shopping', color: 'base' },
+      { id: crypto.randomUUID(), name: 'Gaming', color: 'base' },
+    ],
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'Tags',
+    tags: [
+      { id: crypto.randomUUID(), name: 'Travel', color: 'base' },
+      { id: crypto.randomUUID(), name: 'Important', color: 'blue' },
+      { id: crypto.randomUUID(), name: 'Sick', color: 'red' },
+    ],
+  },
+]
--- frontend/src/routes/docs/assemblies/entry/+page.md
+++ frontend/src/routes/docs/assemblies/entry/+page.md
@@ -1,10 +1,11 @@
 <script>
 import DocsExample from '$lib/components/utils/DocsExample.svelte'
 import Entry from '$lib/assemblies/Entry.svelte'
+import { defaultCategories } from '$lib/types/log'
 </script>
 
 # Entry
 
 <DocsExample>
-  <Entry />
+  <Entry categories={defaultCategories} />
 </DocsExample>
