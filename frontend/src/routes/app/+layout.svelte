7621a9921abdbde812c9aed58a32eba35a99d511
LIARA <liarabruchmann@gmail.com>
2025-10-17 18:56:17 +0200

    fix: reroute if no session

--- frontend/src/routes/app/+layout.svelte
+++ frontend/src/routes/app/+layout.svelte
@@ -1,7 +1,19 @@
 <script>
+import { goto } from '$app/navigation'
+import { useUserStore } from '$lib/store/userStore.svelte'
+import { onMount } from 'svelte'
+
 let { children } = $props()
 
+let userStore = useUserStore()
+
 const title = 'Ephemeride'
+
+onMount(() => {
+  if (!userStore.sessionId) {
+    goto('/')
+  }
+})
 </script>
 
 <svelte:head>
